4f1ddc20794101b70f6e30a35088c7a767db345a
Glenn Klaas <glennklaas@gmail.com>
2017-11-02 01:48:47 -0400

    sync

--- gaksbc.s
+++ gaksbc.s
@@ -3,14 +3,17 @@
 .debuginfo +
 .setcpu "6502"
 
-;GENERAL EQUATES
+;-------------------------------------------------------------------------
+;  ACIA ADDRESSES
+;-------------------------------------------------------------------------
+
 ACIA 		:= $A000	;BASE ADDRESS OF THE MC68B50P
 ACIAControl 	:= ACIA+0	; Set operational parameters (w)     
 ACIAStatus 	:= ACIA+0	; Indicates if char has been received (r)
-ACIAData 		:= ACIA+1	; Data being recieved (r) or sent (w)
+ACIAData 	:= ACIA+1	; Data being recieved (r) or sent (w)
 
 ;-------------------------------------------------------------------------
-;  Memory declaration
+;  VARIABLES
 ;-------------------------------------------------------------------------
 
 XAML		= $10		;LAST "OPENED" LOCATION LOW
@@ -21,42 +24,44 @@ L		= $14		;HEX VALUE PARSING LOW
 H		= $15		;HEX VALUE PARSING HIGH
 YSAV		= $16		;USED TO SEE IF HEX VALUE IS GIVEN
 MODE		= $17		;$00=XAM, $7F=STOR, $AE=BLOCK XAM
-MSGL		= $18		;MSG START ADDRESS
-MSGH		= $19		;MSG END ADDRESS
-
+STRL		= $18		;STRING START ADDRESS
+STRH		= $19		;STRING END ADDRESS
 IN		= $200		;INPUT BUFFER
 
 ;-------------------------------------------------------------------------
-;  Constants
+;  CONSTANTS 
 ;-------------------------------------------------------------------------
 
 BS		= $08		;CODE-BACKSPACE KEY
 CR		= $0D		;CODE-CARRIAGE RETURN
 LF		= $0A 		;CODE-LINE FEED
 ESC		= $1B		;CODE-ESC KEY
-PROMPT		= '\'		;PROMPT
-STACK_TOP		= $FF		;TOP OF THE STACK
+PROMPT		= '*'		;PROMPT
+STACK_TOP	= $FF		;TOP OF THE STACK
+
+;-------------------------------------------------------------------------
+;  INITIAL SETUP   
+;-------------------------------------------------------------------------
 
 .segment "CODE"
 .org $C000
 
-RESET:
-	CLD	                    ; clear decimal mode
-	LDX #STACK_TOP
-	TXS
+RESET:	CLD 			;CLEAR DECIMAL MODE
+	LDX #STACK_TOP		;SET THE TOP OF STACK VALUE
+	TXS			;INITIALIZE STACK
 	LDA #$95      		;Set ACIA:CLK/16,8-bits,1 stop
-				; RTS low TX INT disabled
-	STA ACIAControl
+				;  RTS low TX INT disabled
+	STA ACIAControl		;INITIALIZE THE ACIA
 
 ;DISPLAY WELCOME MESSAGE
 	LDA #<MSG1		;DETERMINE LOW
-	STA MSGL			;SET LOW
+	STA STRL		;SET LOW
 	LDA #>MSG1		;DETERMINE HI
-	STA MSGH			;SET HI
+	STA STRH		;SET HI
    	JSR SHWMSG		;SHOW WELCOME
 
 ;-------------------------------------------------------------------------
-; The GETLINE process
+;  COMMAND INPUT PROCESSING 
 ;-------------------------------------------------------------------------
 
 SFTRST:	LDA #ESC       		;AUTO ESCAPE
@@ -70,7 +75,7 @@ NOTCR:	CMP #BS        		;BACKSPACE KEY?
 ESCAPE:	LDA #PROMPT    		;PRINT PROMPT CHARACTER
 	JSR ECHO       		;OUTPUT IT
 
-GETLINE:  JSR CRLF			;SEND CRLF TO SCREEN
+GETLINE:  JSR CRLF		;SEND CRLF TO SCREEN
 
 	LDY #$01       		;INITIALIZE TEXT INDEX
 BAKSPACE: DEY
@@ -80,26 +85,28 @@ BAKSPACE: DEY
 	LDA #BS        		;BACKSPACE AGAIN
 	JSR ECHO
 
-NEXTCHAR: JSR RDKEY      		;WAIT FOR KEYPRESS
+NEXTCHAR: JSR RDKEY    		;WAIT FOR KEYPRESS
 	STA IN,Y       		;ADD TO TEXT BUFFER
 	JSR ECHO       		;DISPLAY CHARACTER
 	;JSR PRBYTE    		;DEBUG - PRINT HEX KEYVALUE
 	CMP #CR        		;CR?
 	BNE NOTCR      		;NO.
 
-;LINE RECEIVED, NOW LET'S PARSE IT
+;-------------------------------------------------------------------------
+;  COMMAND EXECUTION PROCESSING
+;-------------------------------------------------------------------------
 
 	LDY #$FF       		;RESET TEXT INDEX
 	LDA #$00       		;FOR XAM MODE
 	TAX            		;0->X
 
-SETSTOR:	ASL			;LEAVES $7B IF SETTING STOR MODE
+SETSTOR:	ASL		;LEAVES $7B IF SETTING STOR MODE
 
-SETMODE:	STA MODE			;SET MODE FLAGS
+SETMODE:	STA MODE	;SET MODE FLAGS
 
 BLSKIP:	INY			;ADVACE TEXT INDEX
 
-NEXTITEM:	LDA IN,Y			;GET CHARACTER
+NEXTITEM:	LDA IN,Y	;GET CHARACTER
 	CMP #CR
 	BEQ GETLINE		;WE'RE DONE IF IT'S CR!
 	CMP #'.'
@@ -111,16 +118,16 @@ NEXTITEM:	LDA IN,Y			;GET CHARACTER
 	BEQ RUN			;RUN THE PROGRAM! FORGET THE REST
 	STX L			;CLEAR INPUT VALUE (X=0)
 	STX H
-	STY YSAV			;SAVE Y FOR COMPARISON
+	STY YSAV		;SAVE Y FOR COMPARISON
 
 ; HERE WE'RE TRYING TO PARSE A NEW HEX VALUE
 
-NEXTHEX:	LDA IN,Y			;GET CHARACTER FOR HEX TEST
-	EOR #$30			;MAP DIGITS 0-9
-	CMP #$0A			;IS IT A DECIMAL DIGIT?
+NEXTHEX:	LDA IN,Y	;GET CHARACTER FOR HEX TEST
+	EOR #$30		;MAP DIGITS 0-9
+	CMP #$0A		;IS IT A DECIMAL DIGIT?
 	BCC DIG			;YES!
-	ADC #$88			;MAP LETTER "A"-"F" TO $FA-FF
-	CMP #$FA			;HEX LETTER?
+	ADC #$88		;MAP LETTER "A"-"F" TO $FA-FF
+	CMP #$FA		;HEX LETTER?
 	BCC NOTHEX		;NO! CHARACTER NOT HEX
 
 DIG:	ASL
@@ -128,8 +135,8 @@ DIG:	ASL
 	ASL
 	ASL
 
-	LDX #$04			;SHIFT COUNT
-HEXSHIFT:	ASL			;HEX DIGIT LEFT, MSB TO CARRY
+	LDX #$04		;SHIFT COUNT
+HEXSHIFT:	ASL		;HEX DIGIT LEFT, MSB TO CARRY
 	ROL L			;ROTATE INTO LSD
 	ROL H			;ROTATE INTO MSD'S
 	DEX			;DONE 4 SHIFTS?
@@ -137,7 +144,7 @@ HEXSHIFT:	ASL			;HEX DIGIT LEFT, MSB TO CARRY
 	INY			;ADVANCE TEXT INDEX
 	BNE NEXTHEX		;ALWAYS TAKEN
   
-NOTHEX:	CPY YSAV			;WAS AT LEAST 1 HEX DIGIT GIVEN?
+NOTHEX:	CPY YSAV		;WAS AT LEAST 1 HEX DIGIT GIVEN?
 	BNE NOESCAPE		;NO! IGNORE ALL,START FROM SCRATCH
 	JMP ESCAPE
 
@@ -151,11 +158,11 @@ NOESCAPE:	BIT MODE
 TONXTITM: JMP NEXTITEM		;GET NEXT COMMAND ITEM
 
 ;-------------------------------------------------------------------------
-;  RUN user's program from last opened location
+;  RUN USERS PROGRAM FROM THE LAST OPENED LOCATION 
 ;-------------------------------------------------------------------------
 
 RUN:	JSR ACTRUN		;RUN USER'S PRORAM
-	JMP SFTRST
+	JMP SFTRST		;SOFT RESET ON RETURN
 
 ACTRUN:	JMP (XAML)
 
@@ -163,12 +170,12 @@ ACTRUN:	JMP (XAML)
 ;  We're not in Store mode
 ;-------------------------------------------------------------------------
 
-NOTSTOR:	BMI XAMNEXT		;B7=0 FOR XAM, 1 FOR BLOCK XAM
+NOTSTOR:	BMI XAMNEXT	;B7=0 FOR XAM, 1 FOR BLOCK XAM
 
 ;WE'RE IN XAM MODE NOW
 
-	LDX #$02			;COPY 2 BYTES
-SETADR:	LDA L-1,X			;COPY HEX DATA TO
+	LDX #$02		;COPY 2 BYTES
+SETADR:	LDA L-1,X		;COPY HEX DATA TO
 	STA STL-1,X		;store index'
 	STA XAML-1,X		;AND TO 'XAM INDEX'
 	DEX			;NEXT OF 2 BYTES
@@ -176,31 +183,31 @@ SETADR:	LDA L-1,X			;COPY HEX DATA TO
 
 ; PRINT ADDR & DATA FROM THIS ADDR, FALL THRU NEXT BNE
 
-NXTPRNT:	BNE PRDATA		;NE MEANS NO ADDRESS TO PRINT
+NXTPRNT:	BNE PRDATA	;NE MEANS NO ADDRESS TO PRINT
 	JSR CRLF
-	LDA XAMH			;OUTPUT HIGH-ORDER BYTE OF ADDR
+	LDA XAMH		;OUTPUT HIGH-ORDER BYTE OF ADDR
 	JSR PRBYTE
-	LDA XAML			;OUTPUT LOW-ORDER BYTE OF ADDR
+	LDA XAML		;OUTPUT LOW-ORDER BYTE OF ADDR
 	JSR PRBYTE
-	LDA #':'			;PRINT COLON
+	LDA #':'		;PRINT COLON
 	JSR ECHO
 
-PRDATA:	LDA #' '			;PRINT SPACE
+PRDATA:	LDA #' '		;PRINT SPACE
 	JSR ECHO
 	LDA (XAML,X)		;GET DATA FROM ADDRESS(X=0)
 	JSR PRBYTE		;OUTPUT IT IN HEX FORMAT
-XAMNEXT:	STX MODE			;0->MODE(XAM MODE)
-	LDA XAML			;SEE IF THERE'S MORE TO PRINT
+XAMNEXT:	STX MODE	;0->MODE(XAM MODE)
+	LDA XAML		;SEE IF THERE'S MORE TO PRINT
 	CMP L
 	LDA XAMH
 	SBC H
 	BCS TONXTITM		;NOT LESS! NO MORE DATA TO OUTPUT
 
-	INC XAML			;INCREMENT 'EXAMING INDEX'
+	INC XAML		;INCREMENT 'EXAMING INDEX'
 	BNE MOD8CHK		;NO CARRY!
 	INC XAMH
 
-MOD8CHK:	LDA XAML			;IF ADDRESS MOD8=0 START NEW LINE
+MOD8CHK:	LDA XAML	;IF ADDRESS MOD8=0 START NEW LINE
 	AND #%00000111		;8 VALUES PER ROW
 	BPL NXTPRNT		;ALWAYS TAKEN
 
@@ -213,7 +220,7 @@ PRBYTE:	PHA			;SAVE A FOR LSD
 	LSR
 	LSR			;MSD TO LSD
 	LSR
-	JSR PRHEX			;OUTPUT HEX DIGIT
+	JSR PRHEX		;OUTPUT HEX DIGIT
 	PLA			;RESTORE A
 
 ; FALL THROUGH TO PRINT HEX ROUTING
@@ -222,11 +229,11 @@ PRBYTE:	PHA			;SAVE A FOR LSD
 ;  Subroutine to print a hexadecimal digit
 ;-------------------------------------------------------------------------
 
-PRHEX:	AND #$0F			;MASK LSD FOR HEX PRINT
-	ORA #'0'			;ADD "0"
+PRHEX:	AND #$0F		;MASK LSD FOR HEX PRINT
+	ORA #'0'		;ADD "0"
 	CMP #'9'+1		;DIGIT?
-	BCC ECHO			;YES, OUTPUT IT
-	ADC #$06			;ADD OFFSET FOR LETTER
+	BCC ECHO		;YES, OUTPUT IT
+	ADC #$06		;ADD OFFSET FOR LETTER
 
 ; Fall through to print routine
 
@@ -234,23 +241,23 @@ PRHEX:	AND #$0F			;MASK LSD FOR HEX PRINT
 ;  Subroutine to print a character to the terminal
 ;-------------------------------------------------------------------------
 
-ECHO:	PHA			;SAVE A
-	JSR COUT			;COUT
-  	PLA			;RESTORE A
+ECHO:	PHA			;PUSH A TO STACK
+	JSR COUT		;COUT
+  	PLA			;PULL A FROM STACK
 	RTS
 
-SHWMSG:	LDY #$0
-PRINT:	LDA (MSGL),Y
-	BEQ DONE
-	JSR ECHO
-	INY
-	BNE PRINT
-DONE:	RTS
+SHWMSG:	LDY #$0			;INITIALIZE COUNTER
+PRINT:	LDA (STRL),Y		;LOAD NEXT CHAR
+	BEQ DONE		;GOTO DONE IF 0, ELSE
+	JSR ECHO		;SEND CHAR TO SCREEN
+	INY			;INCREMENT COUNTER
+	BNE PRINT		;LOOP FOR NEXT CHAR
+DONE:	RTS			;RETURN TO CALLER
 
 CRLF:	LDA #LF       
-	JSR ECHO			;SEND LF
+	JSR ECHO		;SEND LF
 	LDA #CR
-	JSR ECHO			;SEND CR
+	JSR ECHO		;SEND CR
 	RTS
 
 MSG1:	.byte CR,LF
@@ -258,6 +265,10 @@ MSG1:	.byte CR,LF
 	.byte CR,LF
 	.byte "    By Glenn Klaas"
 	.byte CR,LF,CR,LF,0
+
+;-------------------------------------------------------------------------
+; CORE IO HANDLING ROUTINES
+;-------------------------------------------------------------------------
 
 .segment "IOHANDLER"
 .org $FF00
@@ -278,11 +289,11 @@ MONRDKEY: LDA ACIAStatus
 	LDA ACIAData
 	SEC			; Carry set if key available
 	RTS
-NoDataIn:	CLC			; Carry clear if no key pressed
+NoDataIn:	CLC		; Carry clear if no key pressed
 	RTS
 
 RDKEY:	JSR MONRDKEY		;Check if key was pressed
-	BCC RDKEY			;If not, check again
+	BCC RDKEY		;If not, check again
 	RTS
 
 ;-------------------------------------------------------------------------
