7a20012c1b89b0f7408d0b039dc522c4e6cd1e99
Glenn Klaas <klaasdevelopment@gmail.com>
2018-01-30 23:50:21 -0500

    removed binaries

--- krusader/Toolkit/ktk.bat
+++
@@ -1,4 +0,0 @@
-rem java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n -cp ktk.jar;lib/comm.jar krusader.editor.KrusaderEditor
-rem java -cp ktk.jar;lib/comm.jar krusader.editor.KrusaderEditor
-java -cp ktk.jar;lib/RXTXcomm.jar krusader.editor.KrusaderEditor
-rem pause
