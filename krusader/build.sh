33708bdd4122a124bb7db349aee76323e2b58763
Glenn Klaas <glennklaas@gmail.com>
2018-06-14 01:07:26 -0400

    updated krusader build parameter options

--- krusader/build.sh
+++ krusader/build.sh
@@ -1,2 +1,2 @@
 ca65 FULL_Krusader_1.3_ca65.asm -o Krusader_1.3_65C02_ca65.o -l Krusader_1.3_65C02_ca65.lst
-ld65 -C krusader_test.cfg Krusader_1.3_65C02_ca65.o -o krusader.bin -m krusader.map
+ld65 -o krusader.bin -C krusader_test.cfg Krusader_1.3_65C02_ca65.o -m krusader.map
